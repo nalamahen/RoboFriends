5e3d45fb4cb4c7397c35dd169532c13647695964
Nala Mahen <mail2mahen@yahoo.co.uk>
2018-08-30 16:31:54 +0100

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'sincre@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'shanna@melissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'nathan@yesenia.net'
+    }, 
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'julianne.Oconner@kory.org'
+    },   
+    {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Kameren',
+        email: 'kameren@yahoo.com'
+    },
+    {
+        id: 6,
+        name: 'Krish Mahen',
+        username: 'Krish',
+        email: 'krish@yahoo.com'
+    },
+    {
+        id: 7,
+        name: 'Aarthy Mahen',
+        username: 'Aarthy',
+        email: 'aarthy@hotmail.com'
+    },
+    {
+        id: 8,
+        name: 'Suji Mahen',
+        username: 'Suji',
+        email: 'Suji@yahoo.co.uk'
+    },
+    {
+        id: 9,
+        name: 'Siva Suthan',
+        username: 'Siva',
+        email: 'Siva@yahoo.com'
+    },
+    {
+        id: 10,
+        name: 'Nala Mahen',
+        username: 'Nala',
+        email: 'Nala@yahoo.com'
+    }
+];
